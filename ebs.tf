975b8275ec1b5e7c25e3a3d20ca8ab4b0f5dde0d
Daniel Huckstep <daniel@huckstep.ca>
2021-01-16 00:09:28 -0400

    getting it going

---
+++ ebs.tf
@@ -0,0 +1,114 @@
+# https://docs.aws.amazon.com/eks/latest/userguide/ebs-csi.html
+# https://github.com/kubernetes-sigs/aws-ebs-csi-driver
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:CreateSnapshot",
+      "ec2:CreateTags",
+      "ec2:CreateVolume",
+      "ec2:DeleteSnapshot",
+      "ec2:DeleteTags",
+      "ec2:DeleteVolume",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInstances",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeTags",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumesModifications",
+      "ec2:DetachVolume",
+      "ec2:ModifyVolume",
+      "kms:RetireGrant",
+      "kms:CreateGrant",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Encrypt",
+      "kms:DescribeKey",
+      "kms:Decrypt",
+      "kms:GenerateDataKeyWithoutPlaintext"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name        = "Amazon_EBS_CSI_Driver"
+  description = "Let EKS cluster EBS CSI driver make volumes"
+  policy      = data.aws_iam_policy_document.ebs-csi.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = var.worker_iam_role_name
+  policy_arn = aws_iam_policy.ebs-csi.arn
+}
+
+resource "helm_release" "ebs-csi" {
+  name        = "aws-ebs-csi-driver"
+  repository  = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart       = "aws-ebs-csi-driver"
+  version     = var.chart_version
+  max_history = 10
+  namespace   = var.namespace
+
+  set {
+    name  = "enableVolumeResizing"
+    value = var.volume_resizing
+  }
+
+  set {
+    name  = "enableVolumeScheduling"
+    value = var.volume_scheduling
+  }
+
+  dynamic "set" {
+    for_each = var.extra_tags
+    content {
+      name  = "extraVolumeTags.${set.key}"
+      value = set.value
+    }
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = "100m"
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = "100m"
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "k8sTagClusterId"
+    value = var.cluster_id
+  }
+}
+
+resource "kubernetes_storage_class" "this" {
+  metadata {
+    name = var.storage_class_name
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  allow_volume_expansion = true
+  reclaim_policy         = var.reclaim_policy
+  volume_binding_mode    = var.volume_binding_mode
+
+  parameters = {
+    type                        = var.type
+    "csi.storage.k8s.io/fstype" = var.fs_type
+    encrypted                   = "true"
+    kmsKeyId                    = var.kms_key_arn
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+terraform {
+  experiments = [variable_validation]
+}
+
+variable "chart_version" {
+  default     = "0.8.2"
+  description = "The Helm chart version to install"
+}
+
+variable "volume_resizing" {
+  default     = true
+  description = "Whether to allow volume resizing"
+}
+
+variable "volume_scheduling" {
+  default     = true
+  description = "Whether to allow volume scheduling"
+}
+
+variable "cluster_id" {
+  description = "The EKS cluster_id"
+}
+
+variable "storage_class_name" {
+  default     = "fast"
+  description = "The name to give the new StorageClass"
+}
+
+variable "worker_iam_role_name" {
+  description = "The EKS worker IAM role name"
+}
+
+variable "kms_key_arn" {
+  description = "The KMS key ARN to encrypt volumes with"
+}
+
+variable "type" {
+  default     = "gp2"
+  description = "The type of storage to use"
+
+  validation {
+    condition     = contains(["gp3", "gp2", "io2", "io1", "st1", "sc1", "standard"], var.type)
+    error_message = "Must be one of gp3, gp2, io2, io1, st1, sc1, standard."
+  }
+}
+
+variable "fs_type" {
+  default     = "ext4"
+  description = "The filesystem type to use"
+
+  validation {
+    condition     = contains(["ext4", "xfs"], var.fs_type)
+    error_message = "Must be one of ext4 or xfs."
+  }
+}
+
+variable "reclaim_policy" {
+  default     = "Delete"
+  description = "The reclaim policy for the StorageClass"
+
+  validation {
+    condition     = contains(["Delete", "Retain"], var.reclaim_policy)
+    error_message = "Must be one of Delete or Retain."
+  }
+}
+
+variable "volume_binding_mode" {
+  default     = "WaitForFirstConsumer"
+  description = "The volume binding mode for the StorageClass"
+
+  validation {
+    condition     = contains(["WaitForFirstConsumer", "Immediate"], var.volume_binding_mode)
+    error_message = "Must be one of WaitForFirstConsumer or Immediate."
+  }
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "A map of extra volume tags"
+}
+
+variable "namespace" {
+  default     = "kube-system"
+  description = "The k8s namespace to install the driver in"
+}
